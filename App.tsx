25b06ad2e51218dc65d1e31e24164f5be78b3543
jasrzhang <zhangshurui1989@gmail.com>
2023-10-28 11:11:31 +1100

    Added navigation stack

--- App.tsx
+++ App.tsx
@@ -7,13 +7,20 @@
 
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import {View, Text} from 'react-native';
 import CategoriesScreen from './src/screens/CategoriesScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <CategoriesScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
